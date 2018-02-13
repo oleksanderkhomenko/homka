bd06b727ff544db8dd9a4aaa63d55199c4f9f6d6
Oleksandr Khomenko <khomenko.artistsoft@gmail.com>
2018-02-13 11:56:36 +0200

    added styles to photos

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,6 +34,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [50, 50]
   end
 
+  version :thumb_150 do
+    process resize_to_fit: [150, 150]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
